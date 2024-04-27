723ba29e08e8865deabde981663d79c3760a3e9e
Lurrr1 <fadhlur000@gmail.com>
2024-04-27 14:57:37 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <link
+    rel="stylesheet"
+    href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css"
+  />
+  <div id="app">
+    <Header />
+    <Home />
+  </div>
+</template>
+
+<script>
+import Header from "./components/Header.vue";
+import Home from "./components/Home.vue";
+
+export default {
+  name: "App",
+  components: {
+    Header,
+    Home,
+  },
+};
+</script>
