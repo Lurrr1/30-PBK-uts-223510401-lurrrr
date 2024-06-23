6afe3eebc19a023fe93def1a0235ff5f235cf50f
Lurrr1 <fadhlur000@gmail.com>
2024-06-23 19:12:02 +0700

    perbaiki lagi

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import { createApp } from "vue";
 import App from "./App.vue";
-import router from "./router";
+import router from "./Router";
 import { Quasar } from "quasar";
 import "@quasar/extras/material-icons/material-icons.css";
 import "quasar/src/css/index.sass";
